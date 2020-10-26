f6801f3c887703dc021183ffcd7566ca5408872a
Alexander <dr.sanches3@gmail.com>
2020-10-26 21:42:47 +0300

    Added docker-compose

--- auth-service/src/main/java/ru/drsanches/auth_service/data/UserRepository.java
+++ auth-service/src/main/java/ru/drsanches/auth_service/data/UserRepository.java
@@ -1,6 +1,5 @@
 package ru.drsanches.auth_service.data;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
@@ -9,11 +8,4 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, String> {
 
     Optional<User> findByUsername(String username);
-
-    //TODO: is it necessary?
-    @Query("SELECT * FROM user_profile WHERE enabled = true")
-    Optional<User> findEnabled();
-
-    @Query("SELECT * FROM user_profile WHERE enabled = false")
-    Optional<User> findDisabled();
 }
--- user-service/src/main/java/ru/drsanches/user_service/data/user/UserRepository.java
+++ user-service/src/main/java/ru/drsanches/user_service/data/user/UserRepository.java
@@ -1,6 +1,5 @@
 package ru.drsanches.user_service.data.user;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
@@ -10,11 +9,4 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, String> {
 
     Optional<User> findByUsername(String username);
-
-    //TODO: is it necessary?
-    @Query("SELECT * FROM user_profile WHERE enabled = true")
-    Optional<User> findEnabled();
-
-    @Query("SELECT * FROM user_profile WHERE enabled = false")
-    Optional<User> findDisabled();
 }
